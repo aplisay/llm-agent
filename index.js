1c474d911498235ee0bb9d0ae9346ea75974fca8
Rob Pickering <rob@pickering.org>
2023-06-09 17:25:41 +0100

    separate model from agent logic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 const { createServer } = require('http');
 const { createEndpoint } = require('@jambonz/node-client-ws');
-const { llm } = require('./lib/chatgpt35');
+const Gpt35 = require('./lib/gpt35');
+const agent = require('./lib/agent')
 
 const server = createServer();
 const makeService = createEndpoint({server});
@@ -16,7 +17,7 @@ const logger = require('pino')({
 });
 const port = process.env.WS_PORT || 4000;
 
-llm({ logger, makeService });
+agent({ name: 'gpt35', llmClass: Gpt35, logger, makeService });
 
 server.listen(port, () => {
   logger.info(`Server listening at http://localhost:${port}`);
