96082ef42c00a565cc73c579f40560236562ff70
Rob Pickering <rob@pickering.org>
2023-06-16 18:27:37 +0100

    Add Jambonz interface

---
+++ tests/jambonz.test.js
@@ -0,0 +1,62 @@
+const Jambonz = require('../lib/jambonz');
+
+let jambonz;
+
+let testNumber = "02080996900"
+
+let testApplication = {
+  name: "Test Application",
+  url: "wss://aplisay.net/a/b",
+  tts: {
+    vendor: "google",
+    language: "en-GB",
+    voice: "en-GB-Standard-A"
+  },
+  stt: {
+    vendor: 'google',
+    language: "en-GB",
+  }
+
+}
+
+let applicationSid, numberSid;
+
+test('Initialises', () => {
+  jambonz = new Jambonz(require('../lib/logger'), 'user');
+  return expect(jambonz).toBeInstanceOf(Jambonz);
+});
+
+test('List numbers', async () => {
+  return await expect(jambonz.listNumbers()).resolves.toBeInstanceOf(Array); 
+});
+
+
+test('Add application', async () => {
+  return await expect(jambonz.addApplication(testApplication).then(a => ((applicationSid = a.sid),a))).resolves.toHaveProperty('sid');
+});
+
+test('Add number', async () => {
+  return await expect(jambonz.addNumber({ number: testNumber }).then(n => ((numberSid = n.sid), n))).resolves.toHaveProperty('sid');
+});
+
+test('List numbers', async () => {
+  return await expect(jambonz.listNumbers()).resolves.toBeInstanceOf(Array);
+});
+
+test('link application to number', async () => {
+  return await expect(jambonz.updateNumber(numberSid, { application: applicationSid })).resolves;
+});
+
+test('DeleteNumber', async () => {
+  return await expect(jambonz.deleteNumber(numberSid))
+    .resolves;
+});
+
+test('List numbers', async () => {
+  return await expect(jambonz.listNumbers()).resolves.toBeInstanceOf(Array); 
+});
+
+test('Delete Application', async () => {
+  return await expect(jambonz.deleteApplication(applicationSid)).resolves
+});
+
