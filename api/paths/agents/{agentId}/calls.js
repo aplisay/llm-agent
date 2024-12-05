b4cff3303df177cbde843cb8add5b07a5fffd27a
Rob Pickering <rob@pickering.org>
2024-12-05 20:09:57 +0000

    Allow CDRs per user

--- api/paths/agents/{agentId}/calls.js
+++ api/paths/agents/{agentId}/calls.js
@@ -1,3 +1,4 @@
+let { Call, Op } = require('../../../../lib/database');
 let appParameters, log;
 
 module.exports =
@@ -6,18 +7,44 @@ module.exports =
     log = logger;
 
     const callsList = (async (req, res) => {
-      let { agentId } = req.params;
-      let application = Application.recover(agentId);
-      if (!application) {
-        res.status(404).send(`no agent ${agentId}`);
-      }
-      else {
-        calls = Object.entries(application.agent.sessions)
-          .map(([id, call]) => ({id, from: call?.session?.from, to: call?.session?.to }));
-        logger.debug({ application, calls }, 'calls');
-        res.send(calls);
+      try {
+        let { agentId } = req.params;
+        let { startDate, endDate, offset = 0, limit = 50 } = req.query;
+        let where = {
+          [Op.and]: [
+            { agentId },
+            {
+              [Op.or]: [
+                { userId: res.locals.user.id },
+                { organisationId: res.locals.user.organisationId }
+              ]
+            }
+          ]
+        };
+        startDate = startDate ? new Date(startDate) : new Date(0);
+        endDate = endDate ? new Date(endDate) : new Date();
+        if (startDate && endDate) {
+          where.createdAt = {
+            [Op.gte]: startDate,
+            [Op.lte]: endDate
+          };
+        }
+        let { count, rows: calls } = await Call.findAndCountAll({
+          attributes: ['id', 'callerId', 'calledId', 'startedAt', 'endedAt'],
+          where,
+          order: [['createdAt', 'DESC']],
+          limit: parseInt(limit),
+          offset: parseInt(offset)
+        });
+        req.log.debug({ where, count, calls, limit, offset });
+        next = count > (offset + limit) ? offset + limit : false;
+        res.send({ calls, next });
+      } catch (error) {
+        req.log.error(error);
+        res.status(500).send({ error: error.message });
       }
     });
+
     callsList.apiDoc = {
       summary: 'Returns list of calls in progress to this agent',
       operationId: 'callsList',
@@ -28,15 +55,27 @@ module.exports =
           content: {
             'application/json': {
               schema: {
-                type: 'array',
-                items: {
-                  $ref: '#/components/schemas/Call'
+                type: 'object',
+                properties: {
+                  calls: {
+                    type: 'array',
+                    items: {
+                      $ref: '#/components/schemas/Call'
+                    }
+                  },
+                  next: {
+                    type: 'string',
+                    description: 'The offset to use for the next page of results'
+                  }
+                },
+                example: {
+                  calls: [
+                    { id: "648aa45d-204a-4c0c-a1e1-419406254134", from: "+443300889471", to: "+442080996945" },
+                    { id: "632555d87-948e-48f2-a53d-fc5f261daa7", from: "+443300889470", to: "+442080996945" },
+                  ],
+                  next: 3
                 }
-              },
-              example: [
-                { id: "648aa45d-204a-4c0c-a1e1-419406254134", from: "+443300889471", to: "+442080996945" },
-                { id: "632555d87-948e-48f2-a53d-fc5f261daa7", from: "+443300889470", to: "+442080996945" },
-              ]
+              }
             }
           }
         },
@@ -66,11 +105,8 @@ module.exports =
 
 
     return {
-      description: `Call objects are created when an agent receives a call from an external caller
-                    and are destroyed when the dialogue is complete and either agent or caller hang up.
-                    \`Calls\` operations allow listing of live calls, updating agent parameters mid-call
-                    for just one call, and hanging up a call by the agent.`,
-      summary: 'Call objects represent live calls on the agent and may be manipulated to modify a single call',
+      description: `Call objects are created when an agent receives a call from an external caller`,
+      summary: 'Call objects represent live calls on the agent',
       parameters: [
         {
           description: "ID of the parent agent",
