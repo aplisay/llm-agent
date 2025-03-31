c855fa133c1f3382fd2a6bbfef12258761028c3a
Rob Pickering <rob@pickering.org>
2025-03-31 12:56:56 +0100

    Drop mixtral tests failing

--- tests/groq.test.js
+++ tests/groq.test.js
@@ -1,4 +1,3 @@
 const Llm = require('../lib/models/groq.js');
 const prompt = require('../data/defaultPrompts.js')['gpt'];
 require('./lib/llm.js')(Llm, prompt, "llama-3.1-8b-instant");
-require('./lib/llm.js')(Llm, prompt, "mixtral-8x7b-32768");
