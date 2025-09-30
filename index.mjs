d69296000efa8d1be4a07aaafd0fb39fcc3caaba
Rob Pickering <rob@pickering.org>
2025-09-30 23:41:53 +0100

    More cors/server fun

--- index.mjs
+++ index.mjs
@@ -34,6 +34,9 @@ const port = process.env.WS_PORT || 4000;
 if (process.env.NODE_ENV === 'development') {
   apiDoc.servers.unshift({ url: `http://localhost:${port}/api` });
 }
+else if (process.env.K_SERVICE === 'llm-agent-feature') {
+  apiDoc.servers.unshift({ url: `https://llm-agent-feature.aplisay.com/api` });
+}
 else if (process.env.NODE_ENV === 'staging') {
   apiDoc.servers.unshift({ url: `https://llm-agent-staging.aplisay.com/api` });
 }
